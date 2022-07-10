86068cf8d9a74500a6bca69a1110993490f8366b
AndreyKrip <oreal404@mail.ru>
2022-07-10 23:26:19 +0300

    commit...

--- settings.py
+++ settings.py
@@ -1,11 +1,13 @@
 class Settings:
 
     def __init__(self):
-        self.W = 800
-        self.H = 600
-        self.GAME_RUN = True
-        self.FPS = 60
-        self.SPEED = 5
+        self.W = 800  # ширина экрана
+        self.H = 600  # высота экрана
+        self.GAME_RUN = True  # ключ начала основного игрового цикла
+        self.FPS = 60  # частота кадров
+        self.SPEED = 5  # скорость перемещения игрового персонажа
+
+        # Флаги переключения направления движения игрового персонажа
 
         self.left_flag = False
         self.right_flag = False
